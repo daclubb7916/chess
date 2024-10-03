c8ceabd6a628d0e5fbd5740e4a89c343e002373e
Dakota Clubb <dakota.clubb@gmail.com>
2024-10-03 01:40:22 -0600

    implemented getTeamTurn and setTeamTurn methods

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -19,7 +19,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return TeamTurn;
     }
 
     /**
@@ -28,7 +28,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        TeamTurn = team;
     }
 
     /**
