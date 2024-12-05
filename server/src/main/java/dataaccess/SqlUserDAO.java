c6854d06d37ac935e553697e0fd70ec561a8e7e5
Dakota Clubb <dakota.clubb@gmail.com>
2024-12-05 15:16:41 -0700

    implemented configureDatabase() in my SqlUserDAO

--- server/src/main/java/dataaccess/SqlUserDAO.java
+++ server/src/main/java/dataaccess/SqlUserDAO.java
@@ -1,8 +1,15 @@
 package dataaccess;
 
+import exception.ResponseException;
 import model.UserData;
+import java.sql.*;
 
 public class SqlUserDAO implements UserDAO {
+
+    public SqlUserDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
     @Override
     public UserData getUser(String username) throws DataAccessException {
         return null;
@@ -26,5 +33,26 @@ public class SqlUserDAO implements UserDAO {
     @Override
     public void validatePassword(UserData user, String password) throws DataAccessException {
 
+    }
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+
+            var statement = """
+                    CREATE TABLE IF NOT EXISTS users (
+                        id INT NOT NULL AUTO_INCREMENT,
+                        username VARCHAR(255) NOT NULL,
+                        userdata TEXT NOT NULL,
+                        PRIMARY KEY (id)
+                    )""";
+
+            try (var createTableStatement = conn.prepareStatement(statement)) {
+                createTableStatement.executeUpdate();
+            }
+
+        } catch (SQLException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
     }
 }
