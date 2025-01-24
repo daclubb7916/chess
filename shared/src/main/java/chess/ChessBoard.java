49a99722636fc130aecd17b3e4f95a22da75f462
Dakota Clubb <dakota.clubb@gmail.com>
2025-01-23 21:51:28 -0700

    implemented getRow and getCol in ChessPosition, also added private row and col fields

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,7 +8,7 @@ package chess;
  */
 public class ChessBoard {
 
-    private ChessPiece squares[][] = new ChessPiece[8][8];
+    private final ChessPiece[][] squares = new ChessPiece[8][8];
 
     public ChessBoard() {
         
