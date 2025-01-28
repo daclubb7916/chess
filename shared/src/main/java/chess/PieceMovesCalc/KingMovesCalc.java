487ef5ff67197fa49f4ff8991285ab1de877048a
Dakota Clubb <dakota.clubb@gmail.com>
2025-01-27 23:05:31 -0700

    implemented pieceMoves in KingMovesCalc, tests passed

--- shared/src/main/java/chess/PieceMovesCalc/KingMovesCalc.java
+++ shared/src/main/java/chess/PieceMovesCalc/KingMovesCalc.java
@@ -14,6 +14,19 @@ public class KingMovesCalc extends PieceMovesCalc {
 
     @Override
     public Collection<ChessMove> pieceMoves() {
+
+        int rowIndex = myPosition.getRow();
+        int colIndex = myPosition.getColumn();
+
+        singleMove(rowIndex+1, colIndex);
+        singleMove(rowIndex-1, colIndex);
+        singleMove(rowIndex, colIndex+1);
+        singleMove(rowIndex, colIndex-1);
+        singleMove(rowIndex+1, colIndex+1);
+        singleMove(rowIndex+1, colIndex-1);
+        singleMove(rowIndex-1, colIndex+1);
+        singleMove(rowIndex-1, colIndex-1);
+
         return validMoves;
     }
 }
