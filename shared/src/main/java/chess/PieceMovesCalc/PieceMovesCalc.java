89d4e186fb4f462539e4df2067cf84ee7a4cb5f9
Dakota Clubb <dakota.clubb@gmail.com>
2025-01-27 22:41:38 -0700

    Changed PieceMovesCalc to an abstract class so that I could have instance variable and repeat less code. Implemented some common methods such as inBounds, isTeamMate, addToMoves, and isValid

--- shared/src/main/java/chess/PieceMovesCalc/PieceMovesCalc.java
+++ shared/src/main/java/chess/PieceMovesCalc/PieceMovesCalc.java
@@ -1,13 +1,42 @@
 package chess.PieceMovesCalc;
 
 import chess.*;
+
+import java.util.ArrayList;
 import java.util.Collection;
 
-public interface PieceMovesCalc {
+public abstract class PieceMovesCalc {
+    private final ChessBoard board;
+    private final ChessPosition myPosition;
+    private final ChessGame.TeamColor pawnColor;
+    private final ArrayList<ChessMove> validMoves = new ArrayList<>();
 
-    Collection<ChessMove> pieceMoves();
+    public PieceMovesCalc(ChessBoard board, ChessPosition myPosition) {
+        this.board = board;
+        this.myPosition = myPosition;
+        this.pawnColor = board.getPiece(myPosition).getTeamColor();
+    }
 
-    default boolean inBounds(int row, int col) { return row > 0 && row < 9 && col > 0 && col < 9; }
+    public boolean inBounds(int row, int col) {
+        return row > 0 && row < 9 && col > 0 && col < 9;
+    }
 
+    public boolean isTeamMate(ChessPosition otherPosition) {
+        return board.getPiece(myPosition).getTeamColor() == board.getPiece(otherPosition).getTeamColor();
+    }
 
+    public void addToMoves(ChessPosition otherPosition) {
+        ChessMove validMove = new ChessMove(myPosition, otherPosition, null);
+        validMoves.add(validMove);
+    }
+
+    public boolean isValid(ChessPosition otherPosition) {
+        if (!inBounds(otherPosition.getRow(), otherPosition.getColumn())) {
+            return false;
+        }
+        if (board.getPiece(otherPosition) == null) {
+            return true;
+        }
+        return !isTeamMate(otherPosition);
+    }
 }
