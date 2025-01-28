ff8d59955237e3ae1fd643dcfd17e5cfd0f13ae4
Dakota Clubb <dakota.clubb@gmail.com>
2025-01-27 22:23:21 -0700

    Implemented inBounds() method in PieceMovesCalc interface for use in other methods

--- shared/src/main/java/chess/PieceMovesCalc/PieceMovesCalc.java
+++ shared/src/main/java/chess/PieceMovesCalc/PieceMovesCalc.java
@@ -1,4 +1,13 @@
 package chess.PieceMovesCalc;
 
+import chess.*;
+import java.util.Collection;
+
 public interface PieceMovesCalc {
+
+    Collection<ChessMove> pieceMoves();
+
+    default boolean inBounds(int row, int col) { return row > 0 && row < 9 && col > 0 && col < 9; }
+
+
 }
