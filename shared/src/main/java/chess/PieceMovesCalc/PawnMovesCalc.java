150bd7fff7d30acb2624c53f10d92819419edee5
Dakota Clubb <dakota.clubb@gmail.com>
2025-01-27 23:14:52 -0700

    implemented pieceMoves in PawnMovesCalc, as well as other helper methods. All tests passed in RookMoveTests

--- shared/src/main/java/chess/PieceMovesCalc/PawnMovesCalc.java
+++ shared/src/main/java/chess/PieceMovesCalc/PawnMovesCalc.java
@@ -1,19 +1,127 @@
 package chess.PieceMovesCalc;
 
-import chess.ChessBoard;
-import chess.ChessMove;
-import chess.ChessPosition;
+import chess.*;
 
 import java.util.Collection;
 
 public class PawnMovesCalc extends PieceMovesCalc {
+    private final ChessGame.TeamColor pawnColor;
 
     public PawnMovesCalc(ChessBoard board, ChessPosition myPosition) {
         super(board, myPosition);
+        this.pawnColor = board.getPiece(myPosition).getTeamColor();
     }
 
     @Override
     public Collection<ChessMove> pieceMoves() {
+
+        int rowIndex = myPosition.getRow();
+        int colIndex = myPosition.getColumn();
+
+        if (pawnColor == ChessGame.TeamColor.WHITE) {
+            pawnMoveWhite(rowIndex, colIndex);
+        }
+        if (pawnColor == ChessGame.TeamColor.BLACK) {
+            pawnMoveBlack(rowIndex, colIndex);
+        }
         return validMoves;
+    }
+
+    private void pawnMoveWhite(int row, int col) {
+        ChessPosition otherPosition = new ChessPosition(row+1, col);
+        if (isValidForward(otherPosition)) {
+            if (row == 7) {
+                addPromotedMoves(otherPosition);
+            } else {
+                addToMoves(otherPosition);
+                if (row == 2) {
+                    otherPosition = new ChessPosition(row+2, col);
+                    if (isValidForward(otherPosition)) {
+                        addToMoves(otherPosition);
+                    }
+                }
+            }
+        }
+
+        otherPosition = new ChessPosition(row+1, col+1);
+        if (isValidAttack(otherPosition)) {
+            if (row == 7) {
+                addPromotedMoves(otherPosition);
+            } else {
+                addToMoves(otherPosition);
+            }
+        }
+
+        otherPosition = new ChessPosition(row+1, col-1);
+        if (isValidAttack(otherPosition)) {
+            if (row == 7) {
+                addPromotedMoves(otherPosition);
+            } else {
+                addToMoves(otherPosition);
+            }
+        }
+    }
+
+    private void pawnMoveBlack(int row, int col) {
+        ChessPosition otherPosition = new ChessPosition(row-1, col);
+        if (isValidForward(otherPosition)) {
+            if (row == 2) {
+                addPromotedMoves(otherPosition);
+            } else {
+                addToMoves(otherPosition);
+                if (row == 7) {
+                    otherPosition = new ChessPosition(row-2, col);
+                    if (isValidForward(otherPosition)) {
+                        addToMoves(otherPosition);
+                    }
+                }
+            }
+        }
+
+        otherPosition = new ChessPosition(row-1, col+1);
+        if (isValidAttack(otherPosition)) {
+            if (row == 2) {
+                addPromotedMoves(otherPosition);
+            } else {
+                addToMoves(otherPosition);
+            }
+        }
+
+        otherPosition = new ChessPosition(row-1, col-1);
+        if (isValidAttack(otherPosition)) {
+            if (row == 2) {
+                addPromotedMoves(otherPosition);
+            } else {
+                addToMoves(otherPosition);
+            }
+        }
+    }
+
+    private boolean isValidForward(ChessPosition otherPosition) {
+        if (!inBounds(otherPosition.getRow(), otherPosition.getColumn())) {
+            return false;
+        }
+        return board.getPiece(otherPosition) == null;
+    }
+
+    private boolean isValidAttack(ChessPosition otherPosition) {
+        if (!inBounds(otherPosition.getRow(), otherPosition.getColumn())) {
+            return false;
+        }
+        if (board.getPiece(otherPosition) == null) {
+            return false;
+        }
+        return !isTeamMate(otherPosition);
+    }
+
+    private void addPromotedMoves(ChessPosition otherPosition) {
+        ChessMove queenAdd = new ChessMove(myPosition, otherPosition, ChessPiece.PieceType.QUEEN);
+        ChessMove knightAdd = new ChessMove(myPosition, otherPosition, ChessPiece.PieceType.KNIGHT);
+        ChessMove bishopAdd = new ChessMove(myPosition, otherPosition, ChessPiece.PieceType.BISHOP);
+        ChessMove rookAdd = new ChessMove(myPosition, otherPosition, ChessPiece.PieceType.ROOK);
+        validMoves.add(queenAdd);
+        validMoves.add(knightAdd);
+        validMoves.add(bishopAdd);
+        validMoves.add(rookAdd);
     }
 }
