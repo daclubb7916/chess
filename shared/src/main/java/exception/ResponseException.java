9121e6ee65992fc7ea7b1450578e72522b371b23
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-04 14:21:09 -0700

    Implemented data model record classes and ResponseException class

--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -1,7 +1,14 @@
 package exception;
 
-public class ResponseException extends RuntimeException {
-    public ResponseException(String message) {
+public class ResponseException extends Exception {
+    final private int statusCode;
+
+    public ResponseException(int statusCode, String message) {
         super(message);
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
     }
 }
