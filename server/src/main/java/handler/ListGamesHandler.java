e7136a0e96aabee7bab0ed9e5fb35ad10332ff11
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-04 14:52:52 -0700

    Implemented ListGamesHandler

--- server/src/main/java/handler/ListGamesHandler.java
+++ server/src/main/java/handler/ListGamesHandler.java
@@ -1,4 +1,24 @@
 package handler;
 
-public class ListGamesHandler {
+import com.google.gson.Gson;
+import dataaccess.*;
+import exception.ResponseException;
+import request.ListGamesRequest;
+import result.ListGamesResult;
+import service.GameService;
+import spark.*;
+
+public class ListGamesHandler implements Route {
+    private final GameService gameService;
+
+    public ListGamesHandler(UserDAO userDAO, AuthDAO authDAO, GameDAO gameDAO) {
+        this.gameService = new GameService(userDAO, authDAO, gameDAO);
+    }
+
+    @Override
+    public Object handle(Request req, Response res) throws ResponseException {
+        ListGamesRequest request = new ListGamesRequest(req.headers("authorization"));
+        ListGamesResult result = gameService.listGames(request);
+        return new Gson().toJson(result);
+    }
 }
