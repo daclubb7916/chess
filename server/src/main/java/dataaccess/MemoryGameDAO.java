c8351aaf2a4306e985a6da7da9a77e26d4b4d1c5
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-04 14:26:57 -0700

    Implemented MemoryGameDAO

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,59 @@
 package dataaccess;
 
-public class MemoryGameDAO {
+import model.GameData;
+import java.util.*;
+
+public class MemoryGameDAO implements GameDAO {
+    private final HashMap<Integer, GameData> games = new HashMap<>();
+    private int numGames;
+
+    public MemoryGameDAO() {
+        this.numGames = 1;
+    }
+
+    @Override
+    public GameData createGame(GameData game) throws DataAccessException {
+        for (GameData gameData : games.values()) {
+            if (Objects.equals(gameData.gameName(), game.gameName())) {
+                throw new DataAccessException("Name already taken");
+            }
+        }
+        games.put(game.gameID(), game);
+        numGames += 1;
+        return game;
+    }
+
+    @Override
+    public Collection<GameData> listGames() {
+        return new ArrayList<>(games.values());
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException{
+        if (games.containsKey(gameID)) {
+            return games.get(gameID);
+        }
+        throw new DataAccessException("Game not found");
+    }
+
+    @Override
+    public void updateGame(GameData gameData) {
+        games.put(gameData.gameID(), gameData);
+    }
+
+    @Override
+    public void clear() {
+        numGames = 1;
+        games.clear();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return games.isEmpty();
+    }
+
+    @Override
+    public int getNumGames() {
+        return numGames;
+    }
 }
