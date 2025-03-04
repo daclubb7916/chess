1e3e19c2c1c11982434dac084bcd2eee9de671bb
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-04 14:54:07 -0700

    Implemented JoinGameHandler

--- server/src/main/java/handler/JoinGameHandler.java
+++ server/src/main/java/handler/JoinGameHandler.java
@@ -1,4 +1,28 @@
 package handler;
 
-public class JoinGameHandler {
+import com.google.gson.Gson;
+import dataaccess.*;
+import exception.ResponseException;
+import result.JoinGameResult;
+import request.JoinGameRequest;
+import service.GameService;
+import spark.*;
+
+public class JoinGameHandler implements Route {
+    private final GameService service;
+
+    public JoinGameHandler(UserDAO userDAO, AuthDAO authDAO, GameDAO gameDAO) {
+        this.service = new GameService(userDAO, authDAO, gameDAO);
+    }
+
+    @Override
+    public Object handle(Request req, Response res) throws ResponseException {
+        JoinGameRequest request = new Gson().fromJson(req.body(), JoinGameRequest.class);
+        request = new JoinGameRequest(request.playerColor(), request.gameID(), req.headers("authorization"));
+        if (request.playerColor() == null) {
+            throw new ResponseException(400, "Error: bad request");
+        }
+        JoinGameResult result = service.joinGame(request);
+        return new Gson().toJson(result);
+    }
 }
