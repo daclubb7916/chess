b88431ff073bb43155f0daacccf8517eb6e2a596
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-04 14:51:55 -0700

    Implemented LoginHandler

--- server/src/main/java/handler/LoginHandler.java
+++ server/src/main/java/handler/LoginHandler.java
@@ -1,4 +1,24 @@
 package handler;
 
-public class LoginHandler {
+import com.google.gson.Gson;
+import dataaccess.*;
+import exception.ResponseException;
+import request.LoginRequest;
+import result.LoginResult;
+import service.UserService;
+import spark.*;
+
+public class LoginHandler implements Route {
+    private final UserService userService;
+
+    public LoginHandler(UserDAO userDAO, AuthDAO authDAO) {
+        this.userService = new UserService(userDAO, authDAO);
+    }
+
+    @Override
+    public Object handle(Request req, Response res) throws ResponseException {
+        LoginRequest request = new Gson().fromJson(req.body(), LoginRequest.class);
+        LoginResult result = userService.login(request);
+        return new Gson().toJson(result);
+    }
 }
