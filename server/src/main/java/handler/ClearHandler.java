66c2daa442e8ea94271bc5536c6ca816c81ce13d
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-04 14:56:05 -0700

    Implemented ClearHandler

--- server/src/main/java/handler/ClearHandler.java
+++ server/src/main/java/handler/ClearHandler.java
@@ -1,4 +1,22 @@
 package handler;
 
-public class ClearHandler {
+import com.google.gson.Gson;
+import dataaccess.*;
+import exception.ResponseException;
+import result.ClearResult;
+import service.GameService;
+import spark.*;
+
+public class ClearHandler implements Route {
+    GameService gameService;
+
+    public ClearHandler(UserDAO userDAO, AuthDAO authDAO, GameDAO gameDAO) {
+        this.gameService = new GameService(userDAO, authDAO, gameDAO);
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws ResponseException {
+        gameService.clear();
+        return new Gson().toJson(new ClearResult());
+    }
 }
