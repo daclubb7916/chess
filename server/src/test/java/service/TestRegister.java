24284fc6184d31ae22e836f053f584ec7b1f764a
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-04 16:06:41 -0700

    Created empty test classes, implemented TestRegister

---
+++ server/src/test/java/service/TestRegister.java
@@ -0,0 +1,46 @@
+package service;
+
+import dataaccess.*;
+import exception.ResponseException;
+import org.junit.jupiter.api.BeforeEach;
+import request.RegisterRequest;
+import result.RegisterResult;
+import model.UserData;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class TestRegister {
+
+    private MemoryUserDAO userDAO;
+    private MemoryAuthDAO authDAO;
+    private RegisterRequest request;
+    private UserService userService;
+
+    @BeforeEach
+    public void setup() {
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        request = new RegisterRequest("billy", "billy", "billy@aol.com");
+        userService = new UserService(userDAO, authDAO);
+    }
+
+    @Test
+    public void testUserIsAddedCorrectly() {
+        try {
+            RegisterResult result = userService.register(request);
+            UserData userData = userDAO.getUser(result.username());
+            Assertions.assertEquals(result.username(), userData.username());
+        } catch (ResponseException | DataAccessException ex) {
+            Assertions.fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testUserExistsAlready() {
+        UserData userData = new UserData("billy", "billy", "billy@aol.com");
+        userDAO.createUser(userData);
+        ResponseException exception = Assertions.assertThrows(ResponseException.class,
+                () -> userService.register(request), "ResponseException should be thrown");
+        Assertions.assertEquals("Error: already taken", exception.getMessage());
+    }
+}
