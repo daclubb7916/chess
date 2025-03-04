3fdd7278335d765a65e64e02dabc96374702293c
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-04 14:49:49 -0700

    Implemented RegisterHandler

--- server/src/main/java/handler/RegisterHandler.java
+++ server/src/main/java/handler/RegisterHandler.java
@@ -1,4 +1,27 @@
 package handler;
 
-public class RegisterHandler {
+import com.google.gson.Gson;
+import exception.ResponseException;
+import request.RegisterRequest;
+import result.RegisterResult;
+import service.UserService;
+import dataaccess.*;
+import spark.*;
+
+public class RegisterHandler implements Route {
+    private final UserService userService;
+
+    public RegisterHandler(UserDAO userDAO, AuthDAO authDAO) {
+        this.userService = new UserService(userDAO, authDAO);
+    }
+
+    @Override
+    public Object handle(Request req, Response res) throws ResponseException {
+        RegisterRequest request = new Gson().fromJson(req.body(), RegisterRequest.class);
+        if (request.username() == null | request.email() == null | request.password() == null) {
+            throw new ResponseException(400, "Error: bad request");
+        }
+        RegisterResult result = userService.register(request);
+        return new Gson().toJson(result);
+    }
 }
