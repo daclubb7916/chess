68c9444d6672379c4f54be02bb5be01f3cec5698
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-04 14:50:56 -0700

    Implemented LogoutHandler

--- server/src/main/java/handler/LogoutHandler.java
+++ server/src/main/java/handler/LogoutHandler.java
@@ -1,4 +1,24 @@
 package handler;
 
-public class LogoutHandler {
+import com.google.gson.Gson;
+import dataaccess.*;
+import exception.ResponseException;
+import request.LogoutRequest;
+import result.LogoutResult;
+import service.UserService;
+import spark.*;
+
+public class LogoutHandler implements Route {
+    private final UserService userService;
+
+    public LogoutHandler(UserDAO userDAO, AuthDAO authDAO) {
+        this.userService = new UserService(userDAO, authDAO);
+    }
+
+    @Override
+    public Object handle(Request req, Response res) throws ResponseException {
+        LogoutRequest request = new LogoutRequest(req.headers("authorization"));
+        LogoutResult result = userService.logout(request);
+        return new Gson().toJson(result);
+    }
 }
