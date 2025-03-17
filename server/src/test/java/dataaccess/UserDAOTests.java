6e8466c47b13945053785ce4d2d3f3b352e235e4
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-17 12:19:47 -0600

    Created addSomeUsers method to decrease code duplication

--- server/src/test/java/dataaccess/UserDAOTests.java
+++ server/src/test/java/dataaccess/UserDAOTests.java
@@ -1,4 +1,34 @@
 package dataaccess;
 
+import model.UserData;
+import org.junit.jupiter.api.*;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.SQLException;
+
 public class UserDAOTests {
+
+    private static UserDAO userDAO;
+
+    private void addSomeUsers() throws DataAccessException {
+        String[] usernames = {"bernard", "kdot", "gretchen"};
+        String[] passwords = {"b1zn1zz", "1overB0y", "ggP0zers"};
+        String[] emails = {"ber@aol.com", "trey@yahoo.com", "gma@gmail.com"};
+
+        String statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+
+                for (int i = 0; i < 3; i++) {
+                    ps.setString(1, usernames[i]);
+                    ps.setString(2, BCrypt.hashpw(passwords[i], BCrypt.gensalt()));
+                    ps.setString(3, emails[i]);
+                    ps.executeUpdate();
+                }
+
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
+    }
 }
