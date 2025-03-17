98fd8dd4586e1e9356c34bc4f37689efd894a60c
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-17 11:57:18 -0600

    created empty SqlGameDAO methods

--- server/src/main/java/dataaccess/SqlGameDAO.java
+++ server/src/main/java/dataaccess/SqlGameDAO.java
@@ -1,4 +1,61 @@
 package dataaccess;
 
-public class SqlGameDAO {
+import chess.ChessGame;
+import model.GameData;
+import com.google.gson.Gson;
+import java.sql.*;
+import java.util.*;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+public class SqlGameDAO implements GameDAO {
+
+    public SqlGameDAO() throws DataAccessException {
+        String createTableStatement = """
+            CREATE TABLE IF NOT EXISTS games (
+                gameID INT NOT NULL AUTO_INCREMENT,
+                whiteUsername VARCHAR(255) DEFAULT NULL,
+                blackUsername VARCHAR(255) DEFAULT NULL,
+                gameName VARCHAR(255) NOT NULL,
+                game TEXT NOT NULL,
+                PRIMARY KEY (gameID)
+            )
+            """;
+        DatabaseManager.configureDatabase(createTableStatement);
+    }
+
+    @Override
+    public GameData createGame(GameData game) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+
+    @Override
+    public boolean isEmpty() throws DataAccessException {
+        return false;
+    }
+
+    @Override
+    public int getNumGames() {
+        return 0;
+    }
 }
