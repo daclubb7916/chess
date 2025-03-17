7c2dd9d9e6ea38cf7daa82041f67ad39d84f3b33
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-17 12:21:13 -0600

    Created BeforeAll and AfterEach test methods, as well as a test for when a user exists

--- server/src/test/java/dataaccess/UserDAOTests.java
+++ server/src/test/java/dataaccess/UserDAOTests.java
@@ -10,6 +10,29 @@ public class UserDAOTests {
 
     private static UserDAO userDAO;
 
+    @BeforeAll
+    public static void createDatabase() throws DataAccessException {
+        userDAO = new SqlUserDAO();
+    }
+
+    @AfterEach
+    public void emptyTable() throws DataAccessException {
+        userDAO.clear();
+    }
+
+    @Test
+    public void testGetUserWhenUserExists() throws DataAccessException {
+        addSomeUsers();
+        try {
+            UserData expectedUser = userDAO.getUser("kdot");
+            Assertions.assertTrue(BCrypt.checkpw("1overB0y", expectedUser.password()));
+            Assertions.assertEquals("trey@yahoo.com", expectedUser.email());
+        } catch (DataAccessException ex) {
+            Assertions.fail(ex.getMessage());
+        }
+
+    }
+
     private void addSomeUsers() throws DataAccessException {
         String[] usernames = {"bernard", "kdot", "gretchen"};
         String[] passwords = {"b1zn1zz", "1overB0y", "ggP0zers"};
