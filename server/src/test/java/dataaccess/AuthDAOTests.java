c219b0361c602212d81a82ef6bf5502ef926a492
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-17 12:32:30 -0600

    implemented BeforeAll and AfterEach test conditions in AuthDAOTests

--- server/src/test/java/dataaccess/AuthDAOTests.java
+++ server/src/test/java/dataaccess/AuthDAOTests.java
@@ -1,4 +1,21 @@
 package dataaccess;
 
+import model.AuthData;
+import org.junit.jupiter.api.*;
+import java.sql.SQLException;
+import java.util.UUID;
+
 public class AuthDAOTests {
+
+    private static AuthDAO authDAO;
+
+    @BeforeAll
+    public static void createDatabase() throws DataAccessException {
+        authDAO = new SqlAuthDAO();
+    }
+
+    @AfterEach
+    public void emptyTable() throws DataAccessException {
+        authDAO.clear();
+    }
 }
