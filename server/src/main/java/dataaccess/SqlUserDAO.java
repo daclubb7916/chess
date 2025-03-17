84a913cad40791841a4d9784cb3655f2bd9920dd
Dakota Clubb <dakota.clubb@gmail.com>
2025-03-17 11:50:35 -0600

    Working on SqlUserDAO, created empty methods to be implemented

--- server/src/main/java/dataaccess/SqlUserDAO.java
+++ server/src/main/java/dataaccess/SqlUserDAO.java
@@ -1,4 +1,45 @@
 package dataaccess;
 
-public class SqlUserDAO {
+import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+import java.sql.*;
+
+public class SqlUserDAO implements UserDAO {
+
+    public SqlUserDAO() throws DataAccessException {
+        String createTableStatement = """
+            CREATE TABLE IF NOT EXISTS users (
+                username VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                PRIMARY KEY (username)
+            )
+            """;
+        DatabaseManager.configureDatabase(createTableStatement);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+
+    @Override
+    public boolean isEmpty() throws DataAccessException {
+        return false;
+    }
+
+    @Override
+    public void validatePassword(UserData user, String password) throws DataAccessException {
+
+    }
 }
