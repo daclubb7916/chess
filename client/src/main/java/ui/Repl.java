3c9b7944cbd976caed9bfcbeac30c701980ee85b
Dakota Clubb <dakota.clubb@gmail.com>
2025-04-08 08:07:56 -0600

    Set opening prompts

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -7,17 +7,38 @@ public class Repl {
     private final PreLogin preLogin;
     private final PostLogin postLogin;
     private final GamePlay gamePlay;
+    private State state;
 
     public Repl(String serverUrl) {
         preLogin = new PreLogin(serverUrl);
         postLogin = new PostLogin(serverUrl);
         gamePlay = new GamePlay(serverUrl);
+        state = State.SIGNEDOUT;
     }
 
     public void run() {
         System.out.println("♔ This is Chess! Type 'help' to get started ♔");
         Scanner scanner = new Scanner(System.in);
         String result = "";
+        while (!result.equals("quit")) {
+            switch (state) {
+                case SIGNEDOUT -> {
+                    System.out.print("\nLogin ");
+                    printPrompt();
+                }
+                case SIGNEDIN -> {
+                    System.out.print("\nChess ");
+                    printPrompt();
+                }
+                case INGAME -> {
+                    System.out.print("\nChess Game ");
+                    printPrompt();
+                }
+            }
+        }
+    }
 
+    private void printPrompt() {
+        System.out.print(RESET_BG_COLOR + RESET_TEXT_COLOR + ">>> " + SET_TEXT_COLOR_MAGENTA);
     }
 }
