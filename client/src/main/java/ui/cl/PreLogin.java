e8eca6ac561df204d1d051f50a00eb7af0dd6ee7
Dakota Clubb <dakota.clubb@gmail.com>
2025-04-09 02:53:08 -0600

    Implemented eval method for PreLogin

--- client/src/main/java/ui/cl/PreLogin.java
+++ client/src/main/java/ui/cl/PreLogin.java
@@ -1,6 +1,10 @@
 package ui.cl;
 
+import exception.ResponseException;
 import server.ServerFacade;
+
+import java.util.Arrays;
+
 import static ui.EscapeSequences.*;
 
 public class PreLogin implements ClientUI {
@@ -14,7 +18,19 @@ public class PreLogin implements ClientUI {
 
     @Override
     public String eval(String input) {
-        return "";
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
     }
 
     @Override
@@ -26,5 +42,13 @@ public class PreLogin implements ClientUI {
     public void printPrompt() {
         System.out.print("\n" + RESET_BG_COLOR + SET_TEXT_COLOR_LIGHT_GREY);
         System.out.print("Login >>> " + SET_TEXT_COLOR_MAGENTA);
+    }
+
+    private String register(String... params) throws ResponseException {
+        return "";
+    }
+
+    private String login(String... params) throws ResponseException {
+        return "";
     }
 }
