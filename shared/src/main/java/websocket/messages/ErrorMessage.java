0e5720533aa110fa2e9fcbcdffa81a7c891a7046
Dakota Clubb <dakota.clubb@gmail.com>
2025-05-13 21:13:57 -0600

    Created the Client and Server Websocket message classes to extend the existing classes

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,14 @@
+package websocket.messages;
+
+public class ErrorMessage extends ServerMessage {
+    private final String message;
+
+    public ErrorMessage(String errorMessage) {
+        super(ServerMessageType.ERROR);
+        this.message = errorMessage;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
